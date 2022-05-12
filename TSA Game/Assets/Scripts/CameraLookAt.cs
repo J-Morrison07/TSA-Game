815b979b8b401c50bef599167f2eae9b4531e943
Jake Morrison <87330834+Arc3707@users.noreply.github.com>
2022-05-12 11:22:45 -0700

    5/12/22
    add post poss and worked on cam

--- TSA Game/Assets/Scripts/CameraLookAt.cs
+++ TSA Game/Assets/Scripts/CameraLookAt.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraLookAt : MonoBehaviour
 {
+    public Transform player;
+    public Transform player2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class CameraLookAt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.pos.y = player.position.y + player2.position.y / 2;
+        transform.pos.z = player.position.x + player2.position.x / 2;
     }
 }
