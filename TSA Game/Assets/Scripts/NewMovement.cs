7d8326dbd000874ea7008e9cb76c23cdc419c0fd
lcier835 <104859919+lcier835@users.noreply.github.com>
2022-06-03 10:20:35 -0700

    fixed stuff
    grappp

--- TSA Game/Assets/Scripts/NewMovement.cs
+++ TSA Game/Assets/Scripts/NewMovement.cs
@@ -16,12 +16,16 @@ public class NewMovement : MonoBehaviour
 
     public float acceleration = 2;
     public float airControl = 1;
+    public bool limitAirVelocity = false;
+    public Vector3 maxAirVelocity; // leave at 0 to not enforce that axis, a good example would be the y axis
     public float gravity = -9.81f;
     public float jumpStrength = 10;
     public float maxSpeed = 2;
     public float friction = 2;
     public float stepUpDistance = 0.4f;
     public float wallHitDistance = 0.6f;
+    public float grapplingHookStrength = 1;
+    bool grappling = false;
     Vector3 velocity;
     Vector2 input;
     // Start is called before the first frame update
@@ -31,8 +35,8 @@ public class NewMovement : MonoBehaviour
         mainInput.Movement.Movement.performed += ctx => input = ctx.ReadValue<Vector2>();
         mainInput.Movement.Movement.canceled += ctx => input = Vector2.zero;
         mainInput.Movement.Jump.performed += ctx => Jump();
-        //mainInput.Movement.Grapple.performed += ctx => Grapple(true);
-        //mainInput.Movement.Grapple.canceled += ctx => Grapple(false);
+        mainInput.Movement.Grapple.performed += ctx => Grapple(true);
+        mainInput.Movement.Grapple.canceled += ctx => Grapple(false);
     }
 
     private void OnEnable() {
@@ -52,49 +56,84 @@ public class NewMovement : MonoBehaviour
     }
     // Update is called once per frame
     void Update() {
-        if(velocity.y < 0) {
+        input = input.normalized; // makes it so you don't go any faster while moving diagonally
+        if(velocity.y < 0.1f) {
             isGrounded = Physics.CheckSphere(groundCheck.position, stepUpDistance, groundMask);
         }
-
-        if(isGrounded) {
-            //applies friction
-            velocity.x = velocity.x / friction;
-            velocity.z = velocity.z / friction;
-            //moves the player but clamps velocity by the max speed
-            velocity.x = Mathf.Clamp(velocity.x + input.x * acceleration, maxSpeed * -1, maxSpeed);
-            velocity.z = Mathf.Clamp(velocity.z + input.y * acceleration, maxSpeed * -1, maxSpeed);
-            velocity.y = 0;
-            RaycastHit hit;
-            Physics.Raycast(groundCheck.position + makeVector3(0, stepUpDistance, 0), makeVector3(0, -1, 0), out hit, stepUpDistance * 2);
-            transform.position = makeVector3(0, hit.distance * -1, 0);
-        } else {
-            //no clamping for max speed and no clamping for friction
-            velocity.x = velocity.x + input.x * acceleration * airControl;
-            velocity.z = velocity.z + input.y * acceleration * airControl;
-            velocity.y = velocity.y + gravity;
+        if(!grappling){
+            if(isGrounded) {
+                //applies friction
+                velocity.x = velocity.x / friction;
+                velocity.z = velocity.z / friction;
+                //moves the player but clamps velocity by the max speed
+                velocity.x = Mathf.Clamp(velocity.x + input.x * acceleration, maxSpeed * -1, maxSpeed);
+                velocity.z = Mathf.Clamp(velocity.z + input.y * acceleration, maxSpeed * -1, maxSpeed);
+                velocity.y = 0;
+                RaycastHit hit;
+                Physics.Raycast(groundCheck.position + makeVector3(0, stepUpDistance, 0), makeVector3(0, -1, 0), out hit, stepUpDistance * 2);
+                velocity.y = hit.distance * -1 / Time.deltaTime;
+            } else {
+                //no clamping for max speed and no clamping for friction
+                if (limitAirVelocity){
+                    if(!(maxAirVelocity.x != 0 && maxAirVelocity.x < Mathf.Abs(velocity.x))){
+                        velocity.x = velocity.x + input.x * acceleration * airControl;
+                    }
+                    if(!(maxAirVelocity.y != 0 && maxAirVelocity.y < Mathf.Abs(velocity.y))){
+                        velocity.z = velocity.z + input.y * acceleration * airControl;
+                    }
+                    if(!(maxAirVelocity.z != 0 && maxAirVelocity.z < Mathf.Abs(velocity.z))){
+                        velocity.y = velocity.y + gravity;
+                    }
+                }else{    
+                    velocity.x = velocity.x + input.x * acceleration * airControl;
+                    velocity.z = velocity.z + input.y * acceleration * airControl;
+                    velocity.y = velocity.y + gravity;
+                }
+            }
             Vector3 tmpVec;
-            tmpVec.x = input.x / 3;
-            tmpVec.z = input.y / 3;
+            tmpVec.x = Vector3.Normalize(velocity).x / 1.5f;
+            tmpVec.z = Vector3.Normalize(velocity).z / 1.5f;
             tmpVec.y = 0;
             if(Physics.CheckSphere(wallCheck.position + tmpVec, wallHitDistance, groundMask)){// this makes it so if you're up close to a wall with a lot of velocity in that direction, moving in a different direction is still possible
-                velocity.x = 0;
-                velocity.z = 0;
+                    velocity.x = 0;
+                    velocity.z = 0;
             }
+            controller.Move(velocity * Time.deltaTime);
         }
-        controller.Move(velocity * Time.deltaTime);
     }
 
     void Jump() {
-        if (isGrounded)
+        if (isGrounded && !grappling)
         {
-            Debug.Log("jump started");
             isGrounded = false;
             controller.Move(makeVector3(0, 1, 0));
             velocity.y = jumpStrength;
         }
     }
     void Grapple(bool start) {
-        
+        grappling = start;
+        Vector3 grappleDirection;
+        if(!start){ // add " && isGrounded" to make it so you can only grapple on land, i think it's fun to do it in midair but that's just personal prefrence
+            isGrounded = false;
+            grappleDirection.x = input.x;
+            grappleDirection.y = input.y;
+            grappleDirection.z = 0;
+            RaycastHit hitResult;
+            Vector3 tmpVec2; // adds a vector offset so the ray comes from the player's head
+            tmpVec2.x = tmpVec2.y = 0;
+            tmpVec2.z = 0.25f;
+            // Does the ray intersect any objects excluding the player layer
+            bool hitSomething = Physics.Raycast(transform.position + tmpVec2, grappleDirection, out hitResult, 50);
+            if (hitSomething)
+            {
+                velocity.x = grappleDirection.x * grapplingHookStrength * (hitResult.distance * 2);
+                velocity.y = grapplingHookStrength * ((hitResult.distance + 3) * 5) * grappleDirection.z;
+                isGrounded = false;
+                Debug.Log("hit something");
+            } else {
+                //probably an animation here?
+            }
+        }
     }
 
 }
